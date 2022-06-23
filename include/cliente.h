3857c016e58088e21b0178c3a6a80cfc5111a69e
Gabriel Henrique Silva Maia <102741330+Biel-Maia@users.noreply.github.com>
2022-06-23 11:31:27 -0300

    Update cliente.h

--- include/cliente.h
+++ include/cliente.h
@@ -1,1 +1,19 @@
+#ifndef CLIENTE_H
+#define CLIENTE_H
 
+#include "pessoa.h"
+#include "carro.h"
+#include <vector>
+
+class Cliente : public Pessoa {
+private:
+  std::vector<Carro> _vec;
+public:
+  Cliente(
+    std::string nome,
+    unsigned long cpf,
+    std::vector vec
+  );
+};
+
+#endif
