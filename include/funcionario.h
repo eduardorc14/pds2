b33d03a2e52e28c7bcd9fdb5a69e6d50aef038e3
Gabriel Henrique Silva Maia <102741330+Biel-Maia@users.noreply.github.com>
2022-06-23 11:15:45 -0300

    Update funcionario.h

--- include/funcionario.h
+++ include/funcionario.h
@@ -1,1 +1,18 @@
+#ifndef FUNCIONARIOS_H
+#define FUNCIONARIOS_H
 
+#include "pessoa.h"
+
+class Funcionarios : public Pessoa {
+private:
+  double _salario;
+public:
+  Funcionarios(
+    std::string nome,
+    unsigned long cpf,
+    double salario
+  );
+  double get_salario();
+};
+
+#endif
