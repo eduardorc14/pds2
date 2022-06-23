222fa315e2f73da58a3d20c7b6202eb71d63c3fb
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-06-23 19:28:16 -0300

    Add files via upload

---
+++ include/venda.h
@@ -0,0 +1,12 @@
+/**
+    * Data da realização do serviço
+    * Funcionário que realizou a venda (ou finalizou)
+    * Cliente
+    * Carro
+    * Serviços
+        * Tipo
+        * Valor individual
+        * Quantidade
+    * Valor total
+  
+**/
