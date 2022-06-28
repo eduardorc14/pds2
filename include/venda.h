da7b50db862b47455d351a95c1bf8788b6300b45
eduardorc14 <eduardorc14@hotmail.com>
2022-06-28 16:31:23 -0300

    add venda.h

--- include/venda.h
+++ include/venda.h
@@ -10,3 +10,34 @@
     * Valor total
   
 **/
+#ifndef VENDA_H
+#define VENDA_H
+
+#include<vector>
+
+#include "cliente.h"
+#include "servico.h"
+#include "funcionario.h"
+
+class Venda {
+  private:
+    std::string _data;
+    //histórico de vendas
+    std::vector<Venda> _venda;
+  public:
+    //construindo objeto venda
+    Venda(
+      Cliente cliente,
+      Carro carro, 
+      Servico servico,
+      Funcionario funcionario,
+      std::string data
+    );
+    //obtem a data da venda
+    string get_data();
+    //método para calcular o valor da venda
+    double valor_da_venda(Servico servico);
+    //adiciona a venda ao nosso histórico
+    void adicionar_venda(Venda venda);
+};
+#endif
