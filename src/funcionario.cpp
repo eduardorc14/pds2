522e76ebba441fd54a9996df247241948406420b
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-07-07 10:41:08 -0300

    Add comentarios

--- src/funcionario.cpp
+++ src/funcionario.cpp
@@ -1,11 +1,21 @@
 #include "funcionarios.h"
 
+/**
+*@brief Construtor Class Funcionarios
+*@author Gabriel
+**/
+
 Funcionarios::Funcionarios(
   std::string nome,
   unsigned long cpf,
   double salario
 ): Pessoa(nome, cpf),
    _salario(salario) {};
+
+/**
+*@brief Retorna o salario do Funcionario
+*@return double
+**/
 
 double Funcionarios::get_salario() {
   return _salario;
