2b932a791c4bdd63d303e103303dcaffbd299cd7
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-07-07 10:39:46 -0300

    Add comentarios

--- src/cliente.cpp
+++ src/cliente.cpp
@@ -1,11 +1,21 @@
 #include "cliente.h"
 
+/**
+*@brief Construtor Class Cliente
+*@author Gabriel
+**/
+
 Cliente::Cliente(
   std::string nome,
   unsigned long cpf,
   double salario
 ): Pessoa(nome, cpf),
    _vec(vec) {};
+
+/**
+*@brief Adiciona um novo Carro ao Cliente
+*@param Carro carro
+**/
 
 void Cliente::add_carro_list(Carro carro){
     _vec.push_back(carro);
