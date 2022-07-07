605de6697e5243b4636c6f97aee5dfb601136179
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-07-07 10:37:28 -0300

    Add comentarios e get's

--- src/carro.cpp
+++ src/carro.cpp
@@ -1,10 +1,45 @@
 #include "carro.h"
 
+using namespace std;
+
+/**
+*@brief Construtor Class Carro
+*
+**/
+
 Carro::Carro(
-  std::string marca,
-  std::string modelo,
-  std::string placa
+  string marca,
+  string modelo,
+  string placa
 ): _marca(marca),
    _modelo(modelo),
    _placa(placa) {
 }
+
+/**
+*@brief Retorna a marca do Carro
+*@return string
+**/
+
+string Carro::get_marca(){
+  return _marca;
+}
+
+/**
+*@brief Retorna o modelo do Carro
+*@return string
+**/
+
+string Carro::get_modelo(){
+  return _modelo;
+}
+
+/**
+*@brief Retorna a placa do carro
+*@return string
+**/
+
+string Carro::get_placa(){
+  return _placa;
+}
+
