4fef59ded9ec2dc3d765f582c41f156e1c4e8446
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-07-07 10:32:51 -0300

    Add comentarios

--- src/venda.cpp
+++ src/venda.cpp
@@ -1,16 +1,9 @@
 #include"venda.h"
 
-std::string Venda::get_data(){
-  return _data;
-}
-
-void Venda:: adicionar_venda(Venda venda){
-  _venda.push_back(venda);
-}
-
-double Venda::valor_da_venda(Servico servico){
-  return servico.get_precoServico(); 
-}
+/**
+*@brief Construtor Class Venda
+*@author Eduardo
+**/
 
 Venda::Venda(
   Cliente cliente,
@@ -21,4 +14,34 @@ Venda::Venda(
 ){
     _data = data;
 }
+
+/**
+*@brief Retorna a data que foi efetuada a Venda
+*@return string
+**/
+
+std::string Venda::get_data(){
+  return _data;
+}
+
+/**
+*@brief Adiciona uma nova Venda
+*@param Venda venda
+**/
+
+void Venda:: adicionar_venda(Venda venda){
+  _venda.push_back(venda);
+}
+
+/**
+*@brief Determina o valor da venda de acordo com os valores dos servicos realizados 
+*@param Servico servico
+*@return double
+**/
+
+double Venda::valor_da_venda(Servico servico){
+  return servico.get_precoServico(); 
+}
+
+
   
