b431fea9cdafb652e357073c143a69ed6610d636
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-07-07 10:50:23 -0300

    Add comentarios

--- include/carro.h
+++ include/carro.h
@@ -3,6 +3,11 @@
 
 #include <string>
 
+/**
+*@brief Define os atributos do Carro
+*@author Nilson
+**/
+
 class Carro{
 private:
     std::string _marca;
@@ -10,14 +15,39 @@ private:
     std::string _placa;
 
 public:
+    
+    /**
+    *@brief Construtor Class Carro
+    *@param marca
+    *@param modelo
+    *@param placa
+    **/
+    
   Carro(
     std::string marca,
     std::string modelo,
     std::string placa
   );
     
+    /**
+    *@brief Retorna a marca do Carro
+    *@return string
+    **/
+    
     std::string get_marca();
+    
+    /**
+    *@brief Retorna o modelo do Carro
+    *@return string
+    **/
+    
     std::string get_modelo();
+    
+    /**
+    *@brief Retorna a placa do Carro
+    *@return string
+    **/
+    
     std::string get_placa();
 
 };
