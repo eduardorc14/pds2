1cbc59ca1a2b729bc3166f0c9c8bfcdf28969bb5
Gabriel Henrique Silva Maia <102741330+Biel-Maia@users.noreply.github.com>
2022-07-07 14:39:52 -0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -13,31 +13,30 @@ using namespace std;
 
 void cadastro_x(){
     string nome;
-    unsigned long cpf;
+    string cpf;
     double salario;
     vector <Carro> vec;
     unsigned long telefone;
-    std::string marca;
-    std::string modelo;
-    std::string placa;
+    string marca;
+    string modelo;
+    string placa;
     string tipo;
     double precoServico;
 
-
+    int a=0;
+    while(a==0){
     cout <<"CADASTRAR:" << endl;
     cout <<"1- Cliente" << endl;
     cout <<"2- Funcionario" << endl;
-    cout <<"4- Carro" << endl;
-    cout <<"5- Servico" << endl;
+    cout <<"3- Carro" << endl;
+    cout <<"4- Servico" << endl;
+    cout <<"5- Voltar"<<endl;
     cout <<"Digite uma das opcoes:" << endl;
   
-    int numx = 0;
+    int numx=0;
     cin >> numx;
-    
-    
-    switch(numx){
-        
-        case 1:
+
+    if(numx==1){
         cout <<"Digite o nome:" << endl;
         cin>>nome;
         cout <<"Digite o cpf:" << endl;
@@ -45,58 +44,76 @@ void cadastro_x(){
         cout <<"Digite os carros:" << endl;
         cin<<vec;
         cout <<"Digite o telefone:" << endl;
-        cin<<telefone;
+        cin>>telefone;
         Cliente cliente(nome,cpf,vec,telefone);
+        cout <<"Cliente cadastrado com sucesso:" << endl << endl;
+        a=1;
         break;
+    }
 
-        case 2:
+    if(numx==2){
         cout <<"Digite o nome:" << endl;
         cin>>nome;
         cout <<"Digite o cpf:" << endl;
         cin>>cpf;
         cout <<"Digite o salario:" << endl;
-        cin<<salario;
+        cin>>salario;
         Funcionario funcionario(nome,cpf,salario);
+        cout <<"Funcionario cadastrado com sucesso:" << endl << endl;
+        a=1;
         break;
+    }
 
-        case 3:
+    if(numx==3){
         cout <<"Digite a marca:" << endl;
         cin>>marca;
         cout <<"Digite o modelo:" << endl;
         cin>>modelo;
         cout <<"Digite a placa:" << endl;
-        cin<<placa;
-        Carro Carro(marca,modelo,placa);
+        cin>>placa;
+        Carro carro(marca,modelo,placa);
+        cout <<"Carro cadastrado com sucesso:" << endl << endl;
+        a=1;
         break;
+    }
 
-        case 4:
+    if(numx==4){
         cout <<"Digite o tipo de servico:" << endl;
         cin>>tipo;
         cout <<"Digite o preco:" << endl;
         cin>>precoServico;
         Servico servico(tipo,precoServico);
-
+        cout <<"Servico cadastrado com sucesso:" << endl << endl;
+        a=1;
         break;
-
-        default:
-        cout << "O valor digitado não corresponde a nenhuma opcao!" << endl;
-
     }
 
+    if(numx==5){
+        return;
+    }
 
+    else{
+        cout << "Digite um valor valido!" << endl;
+    }
+    }
 }
 
 
 
 int main(){
+    int x=0;
+    int y=0;
 
+    while(x==0){
+    y=0;
+    while(y==0){
     cout<<"Bem vindo ao sistema de gerenciamento de servicos automotivos!"<<endl;
-
     cout<<"MENU:"<<endl;
     cout<<"1- Cadastro de servicos, clientes, funcionarios ou carros."<<endl;
     cout<<"2) Consultar servicos, clientes, funcionarios ou carros."<<endl;
     cout<<"3) Atualizar servicos, clientes, funcionarios ou carros."<<endl;
     cout<<"4) Efetuar venda"<<endl;
+    cout<<"5) Sair"<<endl;
     
 
     cout<<"Digite uma das opcoes:"<<endl;
@@ -104,28 +121,39 @@ int main(){
     int num = 0;
     cin >> num;
 
-    switch(num){
-        case 1:
-            cadastro_x();
-            break;
-
-        case 2:
-            consulta_x();
-
-            break;
-
-        case 3:
-            atualizar_x();
-            break;
-
-        case 4:
-            
-            break;
-        default:
-            cout << "O valor digitado não corresponde a nenhuma opcao!" << endl;
+    if(num==1){
+    cadastro_x();
+    y=1;
+    break;
     }
 
+    if(num==2){
+    consulta_x();
+    y=1;
+    break;
+    }
+
+    if(num==3){
+    atualizar_x();
+    y=1;
+    break;
+    }
+
+    if(num==4){
+    
+    y=1;
+    break;
+    }
+
+    if(num==5){
+    return 0;
+    }
+    
+    else{
+    cout << "Digite um valor valido!" << endl; 
+    }
+
+    }    
+}
     return 0;
 }
-
-
