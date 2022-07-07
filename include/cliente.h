ea378f9a948a8128908d1d60073944f3a00af6d5
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-07-07 10:53:31 -0300

    Add comentarios

--- include/cliente.h
+++ include/cliente.h
@@ -5,11 +5,27 @@
 #include <vector>
 #include <string>
 
+/**
+*@brief Define os atributos do Cliente
+*@author Gabriel
+**/
+
 class Cliente : public Pessoa {
+  
 private:
   std::vector<Carro> _vec;
   unsigned long _telefone;
+  
 public:
+  
+  /**
+  *@brief Construtor Class Cliente
+  *@param nome
+  *@param cpf
+  *@param vec
+  *@param telefone
+  **/
+  
   Cliente(
     std::string nome,
     unsigned long cpf,
@@ -17,9 +33,20 @@ public:
     unsigned long telefone
   );
   
+  /**
+  *@brief Retorna o telefone do Cliente
+  *@return unsigned long
+  **/
+  
   unsigned long get_telefone();
   
 };
+
+/**
+*@brief Adiciona um novo Carro ao Cliente
+*@param Carro carro
+**/
+
 void add_carro_list(Carro carro);
 
 
