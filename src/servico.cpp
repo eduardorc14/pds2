a49e59d015587fb16675d229724253f5567f853c
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-07-07 10:11:10 -0300

    Add comentarios

--- src/servico.cpp
+++ src/servico.cpp
@@ -2,14 +2,29 @@
 
 using namespace std;
 
+/**
+* @brief Construtor Class Servico
+* @author Ana Carolina
+**/
+
 Servico::Servico(
   string tipo,
   double precoServico
 ): _tipo(tipo), _precoServico(precoServico) {};
 
+/**
+*@brief Retornar o tipo de servico
+*@return string
+**/
+
 string Servico::get_tipo() {
   return _tipo;
 }
+
+/**
+*@brief Retornar o preco do servico
+*@return double
+**/
 
 double Servico::get_precoServico() {
   return _precoServico;
