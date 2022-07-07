a2f1bc413a132fba5ee95f190cba03f54b9a9b45
Ana Carolina Pessamilio <71273121+anaPessamilio@users.noreply.github.com>
2022-07-07 10:57:53 -0300

    Add comentarios

--- include/pessoa.h
+++ include/pessoa.h
@@ -2,17 +2,43 @@
 #define PESSOA_H
 
 #include <string>
+
+/**
+*@brief Define os atributos de uma Pessoa
+*@author Gabriel
+**/
+
 class Pessoa {
 
   private:
     const std::string _nome;
     const unsigned long _cpf;
+  
   public:
+  
+  /**
+  *@brief Construtor Class Pessoa
+  *@param nome
+  *@param cpf
+  **/
+  
     Pessoa(
     std::string nome,
     unsigned long cpf
    );
+  
+  /**
+  *@brief Retorna o nome da Pessoa
+  *@return string
+  **/
+  
     std::string get_nome() const;
+  
+  /**
+  *@brief Retorna o cpf da Pessoa
+  *@return unsigned long
+  **/
+  
     unsigned long get_cpf() const;
 };
 
